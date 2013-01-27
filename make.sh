fcb479c121fb1ce39ea6a1786b47e553dff9c8ae
swarm <swarm@noid.(none)>
2013-01-27 15:56:39 -0500

    Some simple event handling, just built into the multiply button for now

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
-X='/Users/cvick/Dropbox/calculator/calculator'
-cp $X/src/*.html lib/
-cp $X/src/*.css lib/
-coffee -c --output $X/lib $X/src
+cp src/*.html lib/
+cp src/*.css lib/
+coffee -c --output lib src
